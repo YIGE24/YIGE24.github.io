d8d2944f48eeb4cbcd97a8ab5a5018fd3d5a46eb
YIGE24 <149858346+YIGE24@users.noreply.github.com>
2026-03-06 09:26:40 +0000

    Deploying to gh-pages from @ YIGE24/YIGE24.github.io@0977567607a1b9574a9981367190f47826b429ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,41 +16,27 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+        },{id: "nav-news",
+          title: "News",
           description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-joined-encode-lab-at-westlake-university-as-a-visiting-student-advised-by-prof-huan-wang-excited-to-work-on-efficient-ai-systems",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
+        },{id: "news-joined-encode-lab-at-westlake-university-as-a-visiting-student-advised-by-prof-huan-wang-excited-to-work-on-efficient-ai-systems",
           title: '🚀 Joined ENCODE Lab at Westlake University as a visiting student, advised by...',
+          description: "",
+          section: "News",},{id: "news-thrilled-to-receive-a-ph-d-offer-from-prof-huan-wang-at-westlake-university-excited-to-continue-working-with-the-amazing-team-at-encode-lab",
+          title: '🎉 Thrilled to receive a Ph.D. offer from Prof. Huan Wang at Westlake...',
           description: "",
           section: "News",},{id: "news-graduated-with-my-m-sc-in-cs-from-cuhk-huge-thanks-to-my-advisor-prof-bei-yu-and-collaborators-rui-ming-and-haoyuan-wu-next-stop-westlake-university",
           title: '🎓 Graduated with my M.Sc. in CS from CUHK! Huge thanks to my...',
           description: "",
           section: "News",},{id: "news-our-paper-streamingtom-streaming-token-compression-for-efficient-video-understanding-is-on-arxiv-thanks-to-co-authors-keda-tao-kele-shao-and-prof-huan-wang-check-it-out-here",
           title: '📄 Our paper “StreamingTOM: Streaming Token Compression for Efficient Video Understanding” is on...',
+          description: "",
+          section: "News",},{id: "news-our-paper-streamingtom-streaming-token-compression-for-efficient-video-understanding-has-been-accepted-to-cvpr-2026-thanks-to-all-co-authors-paper",
+          title: '🎉 Our paper “StreamingTOM: Streaming Token Compression for Efficient Video Understanding” has been...',
           description: "",
           section: "News",},{
         id: 'social-email',
@@ -65,6 +51,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/YIGE24", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/xychen24", "_blank");
         },
       },{
         id: 'social-rss',
